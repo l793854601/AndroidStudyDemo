e5ba628a0165d840c768a03d2595cbb54bc05f30
刘德龙 <793854601@qq.com>
2022-10-18 17:48:44 +0800

    基于apt实现Activity传值

---
+++ APTDemo/app/src/main/java/com/tkm/aptdemo/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.tkm.aptdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onUserClicked(view: View) {
+
+    }
+
+    fun onRepositoryClicked(view: View) {
+
+    }
+}
